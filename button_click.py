44f5dc16fc7d9ca1f3feca9b903d2acec4b568c8
ViktoriaKukh <bstet@mail.ru>
2022-12-27 11:44:13 +0500

    First vers

---
+++ button_click.py
@@ -0,0 +1,33 @@
+from funcs import download_audio, download_video
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+# def buttonclick (self):
+#     self.Start_button.clicked.connect(start_button_click(self))
+#     self.Directorie_button.clicked.connect(button_directorie_click(self))
+
+def start_button_click(self):
+    if len(self.video_link.toPlainText()) > 10:
+        if len(self.path_file.toPlainText()) > 0:
+            link = self.video_link.toPlainText()
+            path = self.path_file.toPlainText()
+            if self.radioButton_video.isChecked():
+                download_video(link, path)
+            else:
+                download_audio(link, path)
+        else:
+            QtWidgets.QMessageBox.warning(self.frame,"Ошибка!","Вы не выбрали папку для сохранения!")
+    else:
+        QtWidgets.QMessageBox.warning(self.frame ,"Ошибка!","Укажите ссылку на видео!")
+
+# def start_button_click(self):
+#     link = self.video_link.toPlainText()
+#     path = self.path_file.toPlainText()
+#     if self.radioButton_video.isChecked():
+#         download_video(link, path)
+#     else:
+#         download_audio(link, path)
+
+
+def button_directorie_click(self):
+    directorie_path = QtWidgets.QFileDialog.getExistingDirectory(parent= self.frame, caption='Choose directorie: ',directory=r'C:\Users\User\Desktop' )
+    self.path_file.setPlainText(directorie_path)
---
+++ main.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets
+from inter import Ui_Form
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Frame = QtWidgets.QFrame()
+    ui = Ui_Form()
+    ui.setupUi(Frame)
+    Frame.show()
+    sys.exit(app.exec_())
